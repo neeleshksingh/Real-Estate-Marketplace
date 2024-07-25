b1cb97b55da94d93eb954d45b0a8da69309b05f5
Neelesh Kumar Singh <108358262+neeleshksingh@users.noreply.github.com>
2024-07-25 17:08:04 +0530

    configured oAuth

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { SignUp, SignIn } = require("../controllers/auth.controller");
+const { SignUp, SignIn, google, signOut } = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post('/signUp', SignUp);
 router.post('/login', SignIn);
+router.post('/google', google);
+router.get('/signout', signOut);
 
 module.exports = router;
