482afb1e848330b26997514b5a2167174a4efff6
Neelesh Kumar Singh <108358262+neeleshksingh@users.noreply.github.com>
2024-07-25 11:21:34 +0530

    configured tailwind css and established folder structure.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+import About from './pages/About'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Logout />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
